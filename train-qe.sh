1c87a086e6d0296c382a61e1583113633e9547d9
rajnath <patelrajnath@gmail.com>
2017-05-19 00:11:40 +0530

    updated shell scripts using character as a feature

--- train-qe.sh
+++ train-qe.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python train.py \
+THEANO_FLAGS=optimizer=None python train.py \
 --data_train data/qe/train/train.src.lc data/qe/train/train.mt.lc data/qe/train/train.align \
 --data_train_y data/qe/train/train.tags \
 --data_test data/qe/test/test.src.lc data/qe/test/test.mt.lc data/qe/test/test.align \
@@ -8,10 +8,13 @@ python train.py \
 --data_valid data/qe/dev/dev.src.lc data/qe/dev/dev.mt.lc data/qe/dev/dev.align \
 --data_valid_y data/qe/dev/dev.tags \
 --dictionaries data/qe/train/train.src.lc.json data/qe/train/train.mt.lc.json \
+--character2index data/qe/train/train.src.lc.dict_char.json data/qe/train/train.mt.lc.dict_char.json \
 --label2index data/qe/train/train.tags.json \
 --embeddings data/qe/ep_qe.en.vector.txt data/qe/ep_qe.de.vector.txt \
+--use_model GRU \
 --saveto \
 --use_quest \
-#--use_bilingual #\
+--use_char \
+#--use_bilingual \
 #--use_pretrain # to enable this flag you need to have pretrained word embeddings from word2vec (text)
 
--- train-tag.sh
+++ train-tag.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 python train.py \
+--data_train data/pos/icon2016/hi-en.train.txt \
+--data_train_y data/pos/icon2016/hi-en.train.tags \
+--data_test data/pos/icon2016/hi-en.test.txt \
+--data_test_y data/pos/icon2016/hi-en.test.tags \
+--data_valid data/pos/icon2016/hi-en.dev.txt \
+--data_valid_y data/pos/icon2016/hi-en.dev.tags \
+--dictionaries data/pos/icon2016/hi-en.train.txt.json \
+--character2index data/pos/icon2016/hi-en.train.txt.dict_char.json \
+--label2index data/pos/icon2016/hi-en.train.tags.json \
+--use_model GRU \
+--use_char \
+--saveto \
+--use_tag
+
+<< comments
+python train.py \
 --data_train data/pos/hi-en.train.txt \
 --data_train_y data/pos/hi-en.train.tags \
 --data_test data/pos/hi-en.test.txt \
@@ -9,5 +25,7 @@ python train.py \
 --data_valid_y data/pos/hi-en.dev.tags \
 --dictionaries data/pos/hi-en.train.txt.json \
 --label2index data/pos/hi-en.train.tags.json \
+--use_model GRU \
 --saveto \
 --use_tag 
+comments
