4791fede3e73ab6ec02abdc529e27ca415de69f5
rajnath <patelrajnath@gmail.com>
2017-06-02 12:21:35 +0530

    added LSTM model

--- test-qe.sh
+++ test-qe.sh
@@ -5,6 +5,20 @@ python test.py \
 --data_test_y data/qe/test/test.tags \
 --dictionaries data/qe/train/train.src.lc.json data/qe/train/train.mt.lc.json \
 --embeddings data/qe/pretrain/ep_qe.en.vector.txt data/qe/pretrain/ep_qe.de.vector.txt \
+--character2index data/qe/train/train.src.lc.dict_char.json data/qe/train/train.mt.lc.dict_char.json \
 --label2index data/qe/train/train.tags.json \
---load_model quest.GRU_adadelta
+--load_model quest.LSTM_adadelta_bilingual_pretrain
+#--load_model quest.LSTM_pretrain
+#--load_model quest.LSTM_adadelta_bilingual
+#--load_model quest.LSTM_adadelta
+#--load_model quest.LSTM
+#--load_model quest.GRU_adadelta_char_bilingual_pretrain
+#--load_model quest.GRU_adadelta_bilingual_pretrain
+#--load_model quest.GRU_adadelta_pretrain
+#--load_model quest.GRU_pretrain
+#--load_model quest.GRU_adadelta_char_bilingual
+#--load_model quest.GRU_adadelta_bilingual
+#--load_model quest.GRU_adadelta_char
+#--load_model quest.GRU_adadelta
+#--load_model quest.GRU
 
--- test-tag.sh
+++ test-tag.sh
@@ -4,5 +4,6 @@ python test.py \
 --data_test data/pos/hi-en.test.txt \
 --data_test_y data/pos/hi-en.test.tags \
 --dictionaries data/pos/hi-en.train.txt.json \
+--character2index data/pos/icon2016/hi-en.train.txt.dict_char.json \
 --label2index data/pos/hi-en.train.tags.json \
---load_model tag.GRU_adadelta
+--load_model tag.GRU_adadelta_char
